b84dca50b179b143cfd232f96fc247201ad813ff
eduardoreicherts <dudureichert86@gmail.com>
2023-10-17 21:52:26 -0300

    script

---
+++ Script/script.js
@@ -0,0 +1,6 @@
+let changeTemeBtn = document.querySelector('#change_teme');
+let mainPage = document.querySelector('#main_page')
+
+changeTemeBtn.addEventListener("click", () => {
+    mainPage.style.background = "radial-gradient(rgb(41, 41, 41) , rgb(0, 27, 85))"
+});
